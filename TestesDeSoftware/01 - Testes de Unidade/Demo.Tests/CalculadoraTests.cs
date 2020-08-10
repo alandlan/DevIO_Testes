74813078074555b4f437974d8616c441ca509c10
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-10 19:19:13 -0300

    .

---
+++ TestesDeSoftware/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculador = new Calculadora();
+
+            // Act
+            var resultado = calculador.Somar(2, 2);
+
+            // Assert
+            Assert.Equal(4, resultado);
+        }
+    }
+}
