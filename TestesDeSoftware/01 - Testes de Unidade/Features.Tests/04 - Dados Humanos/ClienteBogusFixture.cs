b7ae489cd11f1c27e040f3d4bea02bf357859125
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-17 20:20:32 -0300

    Utilizando o Bogus

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/04 - Dados Humanos/ClienteBogusFixture.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteBogusFixture> { }
+    public class ClienteBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f=> new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80,DateTime.Now.AddYears(-18)),
+                    "",
+                    true,
+                    DateTime.Now))
+                .RuleFor(c=>c.Email,(f,c)=>f.Internet.Email(c.Nome.ToLower(),c.Sobrenome.ToLower()));
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(1)),
+                    "",
+                    false,
+                    DateTime.Now));
+
+            return cliente;
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
