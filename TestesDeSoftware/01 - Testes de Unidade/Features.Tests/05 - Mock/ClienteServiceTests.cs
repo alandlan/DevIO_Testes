acb393d81fa07aac9cf5792add6b95ab81b8dbb3
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-19 19:09:05 -0300

    Utlizando Mock (Part 3)

--- TestesDeSoftware/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -41,15 +41,43 @@ namespace Features.Tests
         [Trait("Categoria", "Cliente Service Mock Tests")]
         public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
         {
-            Assert.True(false);
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            //Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
         }
 
         [Fact(DisplayName = "Obter Clientes Ativos")]
         [Trait("Categoria", "Cliente Service Mock Tests")]
         public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
         {
-            Assert.True(false);
+            // Arrange
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
 
+            clienteRepo.Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsBogus.ObterClientesVariados());
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+
+            // Assert
+            clienteRepo.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
         }
     }
 }
