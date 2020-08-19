e99fbb2a43a4a7671425015b12811965f0609778
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-19 18:33:39 -0300

    Utilizando o Mock (Part 1)

---
+++ TestesDeSoftware/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Threading;
+using Features.Clientes;
+using MediatR;
+using Moq;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteServiceTests
+    {
+        readonly ClienteBogusFixture _clienteTestsBogus;
+
+        public ClienteServiceTests(ClienteBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsBogus = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            Assert.True(false);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            //Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            Assert.True(false);
+
+        }
+    }
+}
