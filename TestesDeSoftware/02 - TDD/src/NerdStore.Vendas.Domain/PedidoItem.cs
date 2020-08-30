6522f16900fc87409b314dcc14d4278977be1eda
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-30 18:48:05 -0300

    Iniciando o projeto com Test Driven Development(TDD)

---
+++ TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorUnitario { get; set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+    }
+}
