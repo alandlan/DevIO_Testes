8336dee81bf4feb7569766b82729f29be40bcc80
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-08-30 19:31:26 -0300

    Continuando TDD

--- TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ TestesDeSoftware/02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -18,5 +18,15 @@ namespace NerdStore.Vendas.Domain
             Quantidade = quantidade;
             ValorUnitario = valorUnitario;
         }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade = Quantidade + unidades;
+        }
+
+        internal decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
     }
 }
